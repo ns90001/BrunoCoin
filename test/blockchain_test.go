11ff6076d80262bbc44a3c6a1259c439c8f06f64
Ianpyrider <Ianpyrider@users.noreply.github.com>
2021-06-05 14:22:22 -0400

    add Outline

--- test/blockchain_test.go
+++ test/blockchain_test.go
@@ -1,1 +1,49 @@
 package test
+
+import (
+	"BrunoCoin/pkg/block"
+	"BrunoCoin/pkg/block/tx"
+	"BrunoCoin/pkg/block/tx/txi"
+	"BrunoCoin/pkg/block/tx/txo"
+	"BrunoCoin/pkg/blockchain"
+	"encoding/hex"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	genNd := NewGenNd()
+	genNd.Start()
+	genNd.StartMiner()
+
+	testConf := blockchain.DefaultConfig()
+
+	bc := blockchain.New(testConf)
+
+	testTxi := &txi.TransactionInput {
+		TransactionHash: "",
+		OutputIndex:     0,
+		UnlockingScript: "",
+		Amount:          0,
+	}
+
+	testTxo := &txo.TransactionOutput{
+		Amount:        0,
+		LockingScript: "",
+		Liminal:       false,
+	}
+
+	t1 := &tx.Transaction{
+		Version:  0,
+		Inputs:   []*txi.TransactionInput{testTxi},
+		Outputs:  []*txo.TransactionOutput{testTxo},
+		LockTime: 0,
+	}
+
+	transactions := []*tx.Transaction{t1}
+
+	byte1 := byte(0)
+
+	testBlk := block.New(genNd.Chain.LastBlock.Hash(), transactions, hex.EncodeToString([]byte{byte1}))
+
+	bc.Add(testBlk)
+}
